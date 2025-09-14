c15b3c3f094d68ae414a45724de644afa91ce96d
Elaine Ulsh <elaineulsh1126@gmail.com>
2025-09-14 01:11:57 -0400

    Add functionality to the SJF algorithm.

--- src/SJF.java
+++ src/SJF.java
@@ -9,16 +9,51 @@ import java.util.*;
 
 public class SJF extends Algorithm {
 
-    private final Queue<Process> readyQueue = new LinkedList<>();
+    private final Queue<Process> priorityQueue = new PriorityQueue<>(new CustomProcessComparator());
 
     private final Queue<Process> processesToArrive;
+
+    private int now = 0;
 
     public SJF(List<Process> allProcessList){
         super(allProcessList);
         processesToArrive = new LinkedList<>(allProcessList);
     }
 
+    // TODO: The issue currently is that it is returning negative waiting times, which makes
+    //      no sense whatsoever.
+
     @Override
     public void schedule() {
+        System.out.println("Shortest Job First: ");
+
+        while (priorityQueue.isEmpty() || !processesToArrive.isEmpty()) {
+            Process process = processesToArrive.remove();
+            if (now <= process.getArrivalTime()) {
+                now = process.getArrivalTime();
+            }
+            priorityQueue.add(process);
+        }
+
+        Process currentProcess = priorityQueue.remove();
+        int runTime = currentProcess.getBurstTime();
+
+        System.out.println("At time " + now + ": ");
+        CPU.run(currentProcess, runTime);
+
+        now += runTime;
+
+        currentProcess.setRemainingTime(0);
+        currentProcess.setFinishTime(now);
+
+        while(!processesToArrive.isEmpty() && processesToArrive.peek().getArrivalTime() <= now) {
+            priorityQueue.add(processesToArrive.remove());
+        }
+    }
+}
+
+class CustomProcessComparator implements Comparator<Process> {
+    public int compare(Process p1, Process p2) {
+        return p1.getBurstTime() < p2.getBurstTime() ? -1 : 1;
     }
 }
