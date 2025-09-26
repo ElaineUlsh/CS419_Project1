8b66a5c4782ca3d5ccf13c1fb356f93489605309
Elaine Ulsh <elaineulsh1126@gmail.com>
2025-09-26 11:01:59 -0400

    Add the SRTF process functionality.

--- src/SJF.java
+++ src/SJF.java
@@ -1,15 +1,9 @@
 
 import java.util.*;
 
-/**
- * TODO: Implement the non-preemptive SJF (Shortest-Job First) scheduling algorithm.
- *       Non-preemptive SJF: check the lengths of all the jobs in the ready queue
- *          and lets the one with the shortest path process first.
- */
-
 public class SJF extends Algorithm {
 
-    private final Queue<Process> priorityQueue = new PriorityQueue<>(new CustomProcessComparator());
+    private final Queue<Process> priorityQueue = new PriorityQueue<>(new CustomProcessComparatorSJF());
 
     private final Queue<Process> processesToArrive;
 
@@ -19,9 +13,6 @@ public class SJF extends Algorithm {
         super(allProcessList);
         processesToArrive = new LinkedList<>(allProcessList);
     }
-
-    // TODO: The issue currently is that it is returning negative waiting times, which makes
-    //      no sense whatsoever.
 
     @Override
     public void schedule() {
@@ -54,7 +45,7 @@ public class SJF extends Algorithm {
     }
 }
 
-class CustomProcessComparator implements Comparator<Process> {
+class CustomProcessComparatorSJF implements Comparator<Process> {
     public int compare(Process p1, Process p2) {
         return p1.getBurstTime() < p2.getBurstTime() ? -1 : 1;
     }
--- src/SRTF.java
+++ src/SRTF.java
@@ -8,11 +8,85 @@ import java.util.*;
 
 public class SRTF extends Algorithm{
 
-    public SRTF(List<Process> allProcesses){
-        super(allProcesses);
+    private final Queue<Process> priorityQueue = new PriorityQueue<>(new CustomProcessComparatorSRTF());
+
+    private final Queue<Process> processesToArrive;
+
+    private int now = 0;
+
+    public SRTF(List<Process> allProcessesList){
+        super(allProcessesList);
+        processesToArrive = new LinkedList<>(allProcessesList);
     }
 
-    @Override
-    public void schedule(){ }
+    // TODO: The issue is that it's adding things to the priority queue but some of the things haven't arrived yet
+    //  and this is messing with times afterwards
 
+    @Override
+    public void schedule(){
+        System.out.println("Shortest Remaining Time First: ");
+
+        while (!priorityQueue.isEmpty() || !processesToArrive.isEmpty()) {
+            if (priorityQueue.isEmpty()) {
+                Process process = processesToArrive.remove();
+                if (now <= process.getArrivalTime()) {
+                    now = process.getArrivalTime();
+                }
+                priorityQueue.add(process);
+            }
+
+            Process currentProcess = priorityQueue.remove();
+            int runTime = currentProcess.getRemainingTime();
+            int processingTime = runTime;
+            int remainingTime = 0;
+            boolean willFinish = true;
+
+            if (priorityQueue.isEmpty()) {
+                if (!processesToArrive.isEmpty()) {
+                    Process nextProcess = processesToArrive.peek();
+                    int nextArrivalTime = nextProcess.getArrivalTime();
+
+                    if (nextArrivalTime < now + runTime && nextProcess.getBurstTime() < runTime) {
+                        processingTime = nextArrivalTime - now;
+                        remainingTime = runTime - processingTime;
+                        willFinish = false;
+                    }
+                }
+            } else { // could have processes that have already processed a bit
+                Process nextProcess = priorityQueue.peek();
+                int nextArrivalTime = nextProcess.getArrivalTime();
+
+                if ((nextArrivalTime < now + runTime || nextProcess.getBurstTime() != nextProcess.getRemainingTime())
+                        && nextProcess.getRemainingTime() < runTime) {
+                    processingTime = nextArrivalTime - now;
+                    remainingTime = runTime - processingTime;
+                    willFinish = false;
+                    System.out.println(nextArrivalTime);
+                }
+            }
+
+            System.out.print("At time " + now + ": ");
+            CPU.run(currentProcess, processingTime);
+
+            now += processingTime;
+
+            currentProcess.setRemainingTime(remainingTime);
+            if (willFinish) {
+                currentProcess.setFinishTime(now);
+            } else {
+                priorityQueue.add(currentProcess);
+            }
+
+            while(!processesToArrive.isEmpty() && processesToArrive.peek().getArrivalTime() <= now) {
+                priorityQueue.add(processesToArrive.remove());
+            }
+        }
+    }
+
+}
+
+class CustomProcessComparatorSRTF implements Comparator<Process> {
+    public int compare(Process p1, Process p2) {
+        return p1.getRemainingTime() < p2.getRemainingTime() ? -1 : 1;
+    }
 }
